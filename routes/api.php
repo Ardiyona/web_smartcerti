ddfb29aa2a98a1cf3e96d66c440fca9e10a93fb0
Dhiya Rakha Ardiyona <rakhaardiyona98@gmail.com>
2024-12-11 14:51:32 +0700

    perbaikan

--- routes/api.php
+++ routes/api.php
@@ -35,8 +35,8 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::post('/login', App\Http\Controllers\api\LoginController::class)->name('login');
+Route::post('/logout', App\Http\Controllers\api\LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/user', function(Request $request){
     return $request->user();
 });
